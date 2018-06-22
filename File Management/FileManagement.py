4804485c46d34b73f234a571189035df9e96b2bd
ludanxer <ludanxer@gmail.com>
2018-06-22 22:44:43 +0800

    finish logic except restore from disk and message dialog

--- File Management/FileManagement.py
+++ File Management/FileManagement.py
@@ -13,12 +13,13 @@ import sys, os
 from PyQt5.QtWidgets import (QApplication, QWidget, QInputDialog, QFileDialog, 
         QLabel, QPushButton, QMessageBox, QMenu, QAction)
 from PyQt5 import QtCore
+import math
 
 
 class Block():
     def __init__(self):
         self.str = ''
-        self.next = 0
+        self.next = -1
 
 class Node(): # node for dir tree
     def __init__(self, name, fatherNode):
@@ -33,7 +34,6 @@ class FileManagement(QWidget):
     def __init__(self):
         super().__init__()
         self.PATH = '' # path to file management root
-        self.blockList = [] # the list to store 128 blocks
         self.FAT_Bitmap_list = []
         self.ROOT = '' # file tree root
         self.pointer = ''
@@ -41,11 +41,12 @@ class FileManagement(QWidget):
         self.HEIGHT = 30
         self.LENGTH = 500
         self.selectBtn = ''
+        self.storageRemain = 2048
 
-        
+
         self.initUI()
         self.initLogic()
-        
+
         
     def initUI(self):
 
@@ -59,42 +60,38 @@ class FileManagement(QWidget):
         backBtn = QPushButton('返回上一级', self)
         createFileBtn = QPushButton('创建文本文件', self)
         createDirBtn = QPushButton('创建子目录', self)
-        deleteBtn = QPushButton('删除', self)
-        openBtn = QPushButton('打开', self)
+        quitBtn = QPushButton('保存并退出', self)
 
         formatBtn.clicked.connect(self.formatAction)
         backBtn.clicked.connect(self.backAction)
-        createFileBtn.clicked.connect(self.createFileAction)
+        createFileBtn.clicked.connect(self.createTextFileAction)
         createDirBtn.clicked.connect(self.createDirAction)
-        deleteBtn.clicked.connect(self.deleteAction)
-        openBtn.clicked.connect(self.refreshUI)
+        quitBtn.clicked.connect(self.writeIntoDisk)
 
         backBtn.move(30, 40)
         createFileBtn.move(135, 40)
         createDirBtn.move(230, 40)
-        openBtn.move(340, 40)
-        deleteBtn.move(410, 40)
         formatBtn.move(480, 40)
+        quitBtn.move(500, 300)
 
 
         self.loadQss()
-        self.setGeometry(300, 200 ,600, 400)
+        self.setGeometry(300, 200 ,750, 400)
         self.setWindowTitle('File Management')
     
         self.show()
 
     def initLogic(self):
         
-        # self.PATH = QFileDialog.getExistingDirectory(self, 'Choose a file for File Management', '/home') # Add README before pop up 
+        self.PATH = QFileDialog.getExistingDirectory(self, 'Choose a file for File Management', '/home') # Add README before pop up 
         
         # check wether the dir is empty before init!!
-        if os.path.exists(self.PATH + '/.FAT_Bitmap_list'):
+        if os.path.exists(self.PATH + '/.FAT_BitMap_list'):
             pass # restore everything from disk
         else: # init
             for i in range(0, 128):
                 block = Block()
-                self.blockList.append(block) # init blocks
-                self.FAT_Bitmap_list.append(block.next) # init FAT_Bitmap_list
+                self.FAT_Bitmap_list.append(block) # init FAT_Bitmap_list
             self.ROOT = Node('Root' ,None)
             self.pointer = self.ROOT
 
@@ -103,7 +100,7 @@ class FileManagement(QWidget):
         openFileAction = QAction('打开', self)
         deletFileAction = QAction('删除', self)
         openFileAction.triggered.connect(self.openFileAction)
-        deletFileAction.triggered.connect(self.deleteAction)
+        deletFileAction.triggered.connect(self.deleteFileAction)
         self.popMenu.addAction(openFileAction)
         self.popMenu.addAction(deletFileAction)
 
@@ -116,7 +113,9 @@ class FileManagement(QWidget):
         self.setStyleSheet(style_sheet)
 
     def formatAction(self): # delete everything
-        pass
+        self.pointer = self.ROOT
+        self.deleteDir(self.ROOT)
+        self.refreshUI()
 
     def backAction(self): # go to its father node
         if self.pointer.father == None:
@@ -125,10 +124,10 @@ class FileManagement(QWidget):
             self.pointer = self.pointer.father
             self.refreshUI()
 
-    def createFileAction(self):
+    def createTextFileAction(self):
         fileName, ok = QInputDialog.getText(self, 'Input Dialog', 'Please input new file name:')
         
-        if ok and self.checkAvailName(fileName):
+        if ok and str(fileName).isalnum():
             mark = True
             for file in self.pointer.fileList: # look for duplicate name
                 if file[0] == str(fileName):
@@ -138,10 +137,10 @@ class FileManagement(QWidget):
             # check for available space!!
             start = -1
             for i in range(len(self.FAT_Bitmap_list)):
-                if self.FAT_Bitmap_list[i] == 0:
+                if len(self.FAT_Bitmap_list[i].str) == 0:
                     start = i
                     break
-        
+
             if mark:
                 if start == -1: # limited space
                     pass
@@ -156,7 +155,7 @@ class FileManagement(QWidget):
     def createDirAction(self):
         dirName, ok = QInputDialog.getText(self, 'Input Dialog', 'Please input new directory name:')
 
-        if ok and self.checkAvailName(dirName):
+        if ok and str(dirName).isalnum():
             mark = True
             for child in self.pointer.son: # check for duplicate name
                 if child.dirName == str(dirName):
@@ -173,12 +172,6 @@ class FileManagement(QWidget):
         else:
             pass # Please input correct file name
 
-
-    def deleteAction(self):
-        pass
-
-    def openAction(self):
-        pass
 
     def refreshUI(self): # every file or Dir is a pushbtn, cause they will response to click event
         # the file or dir to be displayed are sons of the self.pointer and its fileList
@@ -212,20 +205,44 @@ class FileManagement(QWidget):
             count = count + 1
 
 
-    def checkAvailName(self, string):
-        return True
-
-    def dirDoubelCilckAction(self):
-        pass
-
     def openFileAction(self): # Action in popMenu self.selectBtn
         # self.selectBtn.setStyleSheet('background-color: black')
         if '.txt' in self.selectBtn.text(): # file
             # pop up a text edit dialog && divide into block
-            text, ok = QInputDialog.getMultiLineText(self, '文本文件输入框', '请输入内容')
+            selectedFile = ''
+            for file in self.pointer.fileList:
+                if file[0] == self.selectBtn.text(): # remember to parse filename
+                    selectedFile = file
+                    break
+            originText = self.getText(selectedFile) # get whole text from blocks
+            self.releaseTextFile(selectedFile[1])
+
+            text, ok = QInputDialog.getMultiLineText(self, '文本文件输入框', '请输入内容', originText)
             if ok:
                 length = len(text)
-                
+                if len(text) % 16 ==0:
+                    pass
+                else:
+                    length = math.floor(length/16 + 1) * 16
+
+                if length > self.storageRemain:
+                    pass # 
+                else:
+                    self.storageRemain -= length
+                    blockPointer = self.FAT_Bitmap_list[selectedFile[1]]
+                    for i in range(round(length/16)):
+                        blockPointer.str = text[i*16:(i+1)*16]
+                        if i == (round(length/16)-1):
+                            break
+                        for j in range(len(self.FAT_Bitmap_list)):
+                            if len(self.FAT_Bitmap_list[j].str) == 0: # empty
+                                blockPointer.next = j
+                                blockPointer = self.FAT_Bitmap_list[j]
+                                break
+                # print(self.storageRemain)
+                # for block in self.FAT_Bitmap_list:
+                #     if len(block.str) != 0:
+                #         print(block.str)
             else:
                 pass
         else: # dir
@@ -235,24 +252,87 @@ class FileManagement(QWidget):
                     self.refreshUI()
                     break
 
-    def deletFileAction(self): # Action in popMenu
+    def releaseTextFile(self, startBlockNum):
+        blockPointer = self.FAT_Bitmap_list[startBlockNum]
+        while True:
+            if blockPointer.next == -1:
+                if len(blockPointer.str) == 0: # empty
+                    break
+                else:
+                    blockPointer.str = ''
+                    self.storageRemain += 16
+                    break
+            else:
+                self.storageRemain += 16
+                blockPointer.str = ''
+                tmp = blockPointer.next
+                blockPointer.next = -1
+                blockPointer = self.FAT_Bitmap_list[tmp]
+
+
+    def deleteFileAction(self): # Action in popMenu
         if '.txt' in self.selectBtn.text():
-            pass
+            selectedFile = ''
+            for file in self.pointer.fileList:
+                if file[0] == self.selectBtn.text(): # remember to parse filename
+                    selectedFile = file
+                    break
+            self.releaseTextFile(selectedFile[1])
+            self.pointer.fileList.remove(selectedFile)
+            self.refreshUI()
+            # print(self.storageRemain)
         else:
             for child in self.pointer.son:
                 if child.dirName == self.selectBtn.text(): # text need to add slice !!!
-                    pass # have to delete all files in that dir
+                    # have to delete all files in that dir
+                    self.deleteDir(child)
+                    self.refreshUI()
+                    break
+
 
     def showPopMenu(self, pos):
         source = self.sender()
         self.selectBtn = source
         self.popMenu.exec_(source.mapToGlobal(pos))
 
+    def getText(self, selectedFile):
+        string = ''
+        i = selectedFile[1]
+        while True:
+            string += self.FAT_Bitmap_list[i].str
+            i = self.FAT_Bitmap_list[i].next
+            if i == -1:
+                break
+        return string
+
+    def deleteDir(self, dirNode):
+        for file in dirNode.fileList:
+            self.releaseTextFile(file[1])
+        dirNode.clear()
+        for child in dirNode.son:
+            self.deleteDir(child)
+
     def writeIntoDisk(self):
-        pass # write everything into disk when quiting this system, consider to rm -rf everything and write everything again
+        # write everything into disk when quiting this system, consider to rm -rf everything and write everything again
+        with open(self.PATH + '/.FAT_BitMap_list', 'w') as f:
+            for block in self.FAT_Bitmap_list:
+                f.write(str(block.next) + '\n')
+        self.writeEveryDir(self.ROOT, self.PATH)
+        sys.exit()
 
+    def writeEveryDir(self, dirNode, path):
+        with open(path + '/.index', 'w') as f:
+            for file in dirNode.fileList:
+                f.write(file[0] + ' ' + str(file[1]) + '\n')
 
+        for file in dirNode.fileList:
+            text = self.getText(file)
+            with open(path + '/' + file[0], 'w') as f:
+                f.write(text)
 
+        for child in dirNode.son:
+            os.mkdir(path + '/' + child.dirName)
+            self.writeEveryDir(child, path + '/' + child.dirName)
         
         
 if __name__ == '__main__':
